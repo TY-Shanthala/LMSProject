e1ca9f5ffbd7d133738814fcfaeb1728fb7a42fb
TY-Shanthala <shanthalasajjan1998@gmail.com>
2022-05-04 15:16:00 +0530

    employee module

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import "./style/dateComponent.scss";
 import "./style/color.scss";
 import Login from "./component/pages/login/Login";
 import TextAreaComponent from "./component/atom/TextAreaComponent";
+import EmployeeLayout from "./component/pages/employee/EmployeeLayout";
 
 function App() {
   return (
     <div>
-      {/* <div className="m-5" style={{ width: "30%" }}>
-        <TextAreaComponent />
-      </div> */}
+      {/* <div className="m-5" style={{ width: "30%" }}> */}
       <BrowserRouter>
-        <LayoutComponent />
+        <EmployeeLayout />
       </BrowserRouter>
+      {/* </div> */}
+      {/* <BrowserRouter>
+        <LayoutComponent />
+      </BrowserRouter> */}
       {/* <Login /> */}
     </div>
   );
