61a49c34bc6cff4049eba04ec16f2d4d5cc9587b
MeghaGowda1223456 <meghata22@gmail.com>
2022-05-04 15:19:08 +0530

    Login integration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LayoutComponent from "./component/organism/LayoutComponent";
 import "./style/dateComponent.scss";
 import "./style/color.scss";
 import Login from "./component/pages/login/Login";
 import TextAreaComponent from "./component/atom/TextAreaComponent";
+import ProtectedRoute from "./component/organism/ProtectedRoute";
 
 function App() {
   return (
     <div>
-      {/* <div className="m-5" style={{ width: "30%" }}>
-        <TextAreaComponent />
-      </div> */}
+      {/* <div className="w-100">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/*" />
+            </Route>
+          </Routes>
+
+          {/* <LayoutComponent /> */}
+      {/* <DetailsModal /> */}
+      {/* </BrowserRouter>
+      </div>  */}
+
       <BrowserRouter>
         <LayoutComponent />
       </BrowserRouter>
-      {/* <Login /> */}
     </div>
   );
 }
