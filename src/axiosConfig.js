a42a66ef4713e30eb66bed697bdf172cbed38b27
MeghaGowda1223456 <meghata22@gmail.com>
2022-05-10 17:46:55 +0530

    axios config

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Development URL
-const baseURL = "http://10.10.21.208:3000";
+const baseURL = "http://localhost:3001";
 
 const axiosInstance = axios.create({ baseURL });
 axiosInstance.interceptors.request.use((config) => {
